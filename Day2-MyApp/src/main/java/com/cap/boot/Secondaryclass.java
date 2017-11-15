b93cbcb984352e9fdf6b9613fd65acb6d56edf8d
kamalJas <lnc@DIN76000824.corp.capgemini.com>
2017-11-15 12:09:38 +0530

    Secondary class  changes

--- Day2-MyApp/src/main/java/com/cap/boot/Secondaryclass.java
+++ Day2-MyApp/src/main/java/com/cap/boot/Secondaryclass.java
@@ -5,5 +5,9 @@ public class Secondaryclass {
 	public void doSomething() {
 		System.out.println("Secondary class");
 	}
+	
+	public void doCodeCheckIn() {
+		System.out.println("do code check in...");
+	}
 
 }
